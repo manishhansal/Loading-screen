98f778a850692f5e62ced27ce575a2377a64515f
Manish Kumar <manishhansalmeh@gmail.com>
2022-07-22 18:53:22 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <section className="loading">
+        For new sidebar colors, click your workspace name, then Preferences >
+        Sidebar > Theme
+        <span className="loading__author"> - Your friends at Slack</span>
+        <span className="loading__anim"></span>
+      </section>
+    </div>
+  );
+}
